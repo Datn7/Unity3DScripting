6a9ff68638b792a509594437e1815df2c5780d30
Datn7 <datunashvili72@gmail.com>
2026-04-17 23:48:30 +0400

    components

---
+++ Assets/Scripts/ObjectComponents.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class ObjectComponents : MonoBehaviour
+{
+    Rigidbody rb;
+    PlayerHealth health;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.mass = 10;
+
+
+
+        GetComponent<Renderer>().material.color = Color.plum;
+
+        health = GetComponent<PlayerHealth>();
+
+        
+        health.lives = 9;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int lives = 10;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
