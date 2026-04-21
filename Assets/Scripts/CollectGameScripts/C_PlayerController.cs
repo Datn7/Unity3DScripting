ccca6a8c3d0602068fba1c84987809913149fa52
Datn7 <datunashvili72@gmail.com>
2026-04-22 00:23:59 +0400

    player movement and scene reloading when player fails

--- Assets/Scripts/CollectGameScripts/C_PlayerController.cs
+++ Assets/Scripts/CollectGameScripts/C_PlayerController.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class C_PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float speed = 5f;
+
+    Rigidbody rb;
+
+    float xInput;
+    float yInput;
+
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(transform.position.y < -10)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
+
+        rb.AddForce(xInput, 0, yInput * speed);
     }
 }
