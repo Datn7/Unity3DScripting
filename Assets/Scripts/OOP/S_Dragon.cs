06db04d33665a5f9c401bd2f9165fdb01bf807e1
Datn7 <datunashvili72@gmail.com>
2026-04-24 22:04:22 +0400

    polymorphism

--- Assets/Scripts/OOP/S_Dragon.cs
+++ Assets/Scripts/OOP/S_Dragon.cs
@@ -5,6 +5,21 @@ public class S_Dragon : S_Enemy
     
     public bool isAttacking = false;
 
+
+
+    private void Start()
+    {
+        S_Dragon dragon = new S_Dragon();
+        dragon.Attack();
+
+        S_Dragon dragon2 = new BlueDragon();
+        dragon.Attack();
+
+        S_Dragon dragon3 = new RedDragon();
+        dragon.Attack();
+    }
+
+
     private void Update()
     {
         if (isAttacking)
@@ -13,8 +28,24 @@ public class S_Dragon : S_Enemy
         }
     }
 
-    void Attack()
+    public virtual void Attack()
     {
         print("Enemy attacks for " + damage + " damage!");
+    }
+}
+
+public class  BlueDragon : S_Dragon
+{
+    public override void Attack()
+    {
+        print("Blue Enemy attacks for " + damage + " damage!");
+    }
+}
+
+public class RedDragon : S_Dragon
+{
+    public override void Attack()
+    {
+        print("Red Enemy attacks for " + damage + " damage!");
     }
 }
