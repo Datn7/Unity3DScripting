7bc069658dde1a4b44a1d574b5b9882d3fdbeb98
Datn7 <datunashvili72@gmail.com>
2026-05-05 00:34:04 +0400

    rotation movement

--- Assets/Scripts/RocketGame/Rocket_Movement.cs
+++ Assets/Scripts/RocketGame/Rocket_Movement.cs
@@ -1,9 +1,11 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Rocket_Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
     [SerializeField] float thrustForce = 100f;
 
     Rigidbody rb;
@@ -19,11 +21,29 @@ public class Rocket_Movement : MonoBehaviour
     private void OnEnable()
     {
         thrust.Enable();
+        rotation.Enable();
     }
 
     void FixedUpdate()
     {
-        if(thrust.IsPressed())
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        Debug.Log("here is our rotation value: " + rotationInput);
+
+        if(rotationInput != 0)
+        {
+            rb.AddRelativeTorque(Vector3.forward * rotationInput * thrustForce * Time.deltaTime);
+        }
+    }
+
+    private void ProcessThrust()
+    {
+        if (thrust.IsPressed())
         {
             rb.AddRelativeForce(Vector3.up * thrustForce * Time.deltaTime);
         }
